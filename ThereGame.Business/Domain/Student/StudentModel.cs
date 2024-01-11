9a65c685c77d932834606d32e90098a301194166
Dmitry Ivanov <dmitry.ivanov.developer@gmail.com>
2024-01-11 15:13:48 +0400

    Added CreatedAt to: StudentModel, TeacherModel

--- ThereGame.Business/Domain/Student/StudentModel.cs
+++ ThereGame.Business/Domain/Student/StudentModel.cs
@@ -1,17 +1,17 @@
 namespace ThereGame.Business.Domain.Student;
 
-using System.Text.Json.Serialization;
-using ThereGame.Business.Domain.Dialogue;
 using ThereGame.Business.Domain.Teacher;
 
 
 public class StudentModel 
 {
     public Guid Id { get; set; }
+    public string Avatar { get; set; } = "";
     public string Name { get; set; } = "";
     public string LastName { get; set; } = "";
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
+    public DateTime CreatedAt { get; set; } = new DateTime();
 
     public Guid? TeacherId { get; set; } = null;
     public TeacherModel? Teacher { get; set; } = null;
---
+++ ThereGame.Infrastructure/Data/Migrations/20240111110934_ThereGameMigrationH.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThereGame.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThereGameMigrationH : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Students");
+        }
+    }
+}
